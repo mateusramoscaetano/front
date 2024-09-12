c175493249a261bc603f28be6dda0d17c42a2589
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-12 12:01:21 -0300

    updating dayjs

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -7,10 +7,10 @@ import { Separator } from "./ui/separator";
 import { useQuery } from "@tanstack/react-query";
 import { getSummary } from "../http/get-summary";
 import dayjs from "dayjs";
-import "dayjs/locale/pt-BR";
+import "dayjs/locale/pt-br";
 import { PendingGoals } from "./pending-goals";
 
-dayjs.locale("br");
+dayjs.locale("pt-br");
 
 export function Summary() {
   const { data } = useQuery({
