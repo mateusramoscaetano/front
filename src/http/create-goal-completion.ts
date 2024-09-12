38272bac57acb065b2cb0326960e7d9aeecb982a
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-12 13:03:06 -0300

    server_url env

--- src/http/create-goal-completion.ts
+++ src/http/create-goal-completion.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export async function createGoalCompletion(goalId: string) {
   await fetch("https://back-bbu1.onrender.com/completions", {
     method: "POST",
